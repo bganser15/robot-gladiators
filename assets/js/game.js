6624f5f7973fb0a65798e7997769f807a42f360f
Brittany Ganser <15brganser@gmail.com>
2022-02-15 15:47:09 -0700

    added playerName Variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  var playerName = window.prompt("What is your robot's name?");
+  
+  //fight();
+
+console.log("Our robot's name is " + playerName);

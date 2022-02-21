b46386b8358a2500477925c50f621a8f2d520990
Brittany Ganser <15brganser@gmail.com>
2022-02-21 11:42:22 -0700

    add enemy cobatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,19 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+//enemy names
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+/* for loop example
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
+*/
+var fight = function(enemyName) {
     //alert players they are starting the round
   window.alert("Welcome to Robot Gladiators!");
    
@@ -61,5 +69,10 @@ var fight = function() {
     }
 };
 
+for(var i = 0; i <enemyNames.length; i++) {
+    fight(enemyNames[i]);
+
+}
+
 //run fight to start the game over
-fight();
+//fight();
